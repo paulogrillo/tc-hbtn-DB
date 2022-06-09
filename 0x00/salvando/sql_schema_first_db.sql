3f2d1af927c7c39ba2858be57468ee70d7082844
Paulo Grillo <paulodigitalart@gmail.com>
2022-06-09 16:57:55 -0300

    Task salvando Schema

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE CLIENTE(
+id Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+nome varchar(80) Not NULL,
+mail varchar (40) not NULL,
+telefone int
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
