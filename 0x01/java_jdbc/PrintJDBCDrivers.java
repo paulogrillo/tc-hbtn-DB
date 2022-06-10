eafab5fbabe93cfae7291006c3d68b2a4c2c2e02
Paulo Grillo <paulodigitalart@gmail.com>
2022-06-10 12:29:40 -0300

    Task connectivity JDBC

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -6,20 +6,19 @@ public class PrintJDBCDrivers {
 
     public static void main(String[] args) {
         System.out.println("Lista de drivers JDBC carregados: ");
-        for (Enumeration<Driver> e = DriverManager.getDrivers(); e.hasMoreElements();) {
-            Driver driver = e.nextElement();
+        for (Enumeration<Driver> d = DriverManager.getDrivers(); d.hasMoreElements();) {
+            Driver driver = d.nextElement();
             dadosDoDriver(driver);
         }
     }
-
     public static void dadosDoDriver(Driver driver) {
-        String nomeClasse = driver.getClass().getName();
+        String className = driver.getClass().getName();
         int maiorVersao = driver.getMajorVersion();
         int menorVersao = driver.getMinorVersion();
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("Nome do driver: " + nomeClasse);
+        System.out.println("=================================");
+        System.out.println("Nome do driver: " + className);
         System.out.println("Versão mais recente do driver: " + maiorVersao);
         System.out.println("Versão mais antiga do driver: " + menorVersao);
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("=================================");
     }
 }
